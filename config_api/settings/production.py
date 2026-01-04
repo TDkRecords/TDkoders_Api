eab4354dc178c0c0c1bbf724b866954f954b267c
TDkRecords <tdksongs@gmail.com>
2026-01-04 00:13:01 -0500

    se añade mejor conexión a base de datos

--- config_api/settings/production.py
+++ config_api/settings/production.py
@@ -1,33 +1,16 @@
 from .base import *
 import os
-import dotenv
 import dj_database_url
 
-dotenv.load_dotenv()
 
 DEBUG = False
 
 # Donde vive el api
 ALLOWED_HOSTS = ["api.tdkoders.online"]
 
-""" DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("PGDATABASE"),
-        "USER": os.environ.get("PGUSER"),
-        "PASSWORD": os.environ.get("PGPASSWORD"),
-        "HOST": os.environ.get("PGHOST"),
-        "PORT": os.environ.get("PGPORT", "5432"),
-        "OPTIONS": {
-            "sslmode": "require",
-        },
-    }
-} """
-
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
+        default=os.getenv("DATABASE_URL"),
         ssl_require=True,
     )
 }
